19317165001ec19c3bd1d574c51fc73b4e07dea0
Eugen Borodin <Azsalatay@gmail.com>
2023-03-02 22:49:11 +0100

    Add Switch metod findSeason

--- src/EnumsSeason.java
+++ src/EnumsSeason.java
@@ -20,8 +20,16 @@ public class EnumsSeason {
     DECEMBER,
   }
 
-  public static Map<Month, Integer> buildMonthsMap() { // сделать словарь с месяцами и днями
-    Map<Month, Integer> months = new HashMap<>(); // словарь с месяцами и днями
+  enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    FALL,
+
+  }
+
+  public static Map<Month, Integer> buildMonthsMap() {
+    Map<Month, Integer> months = new HashMap<>();
 
     months.put(Month.JANUARY, 31);
     months.put(Month.FEBRUARY, 28);
@@ -36,34 +44,28 @@ public class EnumsSeason {
     months.put(Month.NOVEMBER, 30);
     months.put(Month.DECEMBER, 31);
 
-    return months; // возвращаем готовый словарь с месяцами и днями
+    return months;
   }
 
-  // программа получает название месяца
-  // возвращает количество дней
+
+
+
   public static void main(String[] args) throws IOException {
     BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
 
-    System.out.print("Enter the month: "); // Введите название месяца
+    System.out.print("Enter the month: ");
     String month = inputReader.readLine();
-
-    // ИмяТипа.valueOf("ЗНАЧЕНИЕ") - вернёт соответствующее ИмяТипа.ЗНАЧЕНИЕ
-    // ИмяТипа.ЗНАЧЕНИЕ.toString() - вернёт строку "ЗНАЧЕНИЕ"
-    Month monthToLook = Month.valueOf(month.toUpperCase()); // месяц для поиска в словаре
+    Month monthToLook = Month.valueOf(month.toUpperCase());
 
     Map<Month, Integer> monthDays = buildMonthsMap();
 
-    // ИмяТипа.ЗНАЧЕНИЕ.ordinal() - вернёт порядковый номер ЗНАЧЕНИЕ в enum, начиная с 0
+
     if (monthDays.containsKey(monthToLook)) {
-      // "Месяц %s содержит %d дней"
       System.out.printf("The month '%s' contains %d days%n", month, monthDays.get(monthToLook));
-      // "У месяца %s вот такой (%d) порядковый номер"
       System.out.printf("The month '%s' has ordinal number %d%n", month, monthToLook.ordinal() + 1);
     } else {
       System.out.println("No such month: " + month); // Нет такого месяца
     }
 
-    // ИмяТипа.values() - возвращает массив всех значений ИмяТипа[] по порядку
-    // Month.values() == Month[]
   }
 }
